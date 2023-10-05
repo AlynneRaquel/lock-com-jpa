1c797d0c0caf9f4835a78c5225eca59bf8e09053
AlynneRaquel <alynneraquel@hotmail.com>
2023-10-05 19:52:09 -0300

    Iniciando projeto

---
+++ src/main/java/com/example/lockjpa/LockJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.lockjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lockjpa/LockJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lockjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
